42b4b76d09fe351168580d0d939801d015f49865
Sabari <r.sabarinathan22@gmail.com>
2021-06-03 18:46:11 +0530

    first commit

---
+++ app/src/main/java/com/andoidsabari/androidphpmysql/Constants.java
@@ -0,0 +1,8 @@
+package com.andoidsabari.androidphpmysql;
+
+public class Constants {
+
+    private static final String ROOT_URL = "https://192.168.29.229/android/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+}
